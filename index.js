41ce679c0eb47646699255ae7086302ef0bb715b
Betinsk <gmdust@htomail.com>
2023-04-14 11:57:54 -0300

    Conexão banco de dados

--- index.js
+++ index.js
@@ -2,12 +2,23 @@ const express = require('express')
 const app = express()
 
 const bodyParser = require('body-parser')
+const connection = require('./database/database')
+//database
+
+connection.authenticate()
+.then(() => {
+    console.log('conexão feita com o banco de dados')
+}).catch((msgErro) => {
+    console.log(msgErro)
+})
 
 //View engine
 app.set('view engine', 'ejs')
 
 app.use(express.static('imagens'));
 app.use(express.static('files'));
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
     res.render('index')
@@ -34,6 +45,10 @@ app.get('/work', (req, res) => {
     res.render('work')
 })
 
+app.post('/register', (req, res) => {
+    var email = req.body.email
+    res.send('register success ' + email)
+})
 
 app.listen(3000, () => {
     console.log('O servidor está rodando!')
