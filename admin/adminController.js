31597d211ad79bf3c645e2a8bbb753a44b674b1d
Betinsk <gmdust@htomail.com>
2023-05-24 13:10:43 -0300

    Edição preso

--- admin/adminController.js
+++ admin/adminController.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const person = require('../database/person')
+const Imate = require('../database/imate')
 
 router.get('/admin/index', (req, res) => {
     res.render('../views/adminIndex')
@@ -8,6 +9,33 @@ router.get('/admin/index', (req, res) => {
 
 router.get('/admin/findPerson', (req, res) => {
     res.render('finds/findPerson')
+})
+
+router.get('/admin/editImate', (req, res) => {
+    res.render('editImate')
+})
+
+//Methods for editin imates
+
+router.get('/admin/editImate/:id', (req, res) => {
+    var id = req.params.id
+
+    if(isNaN(id)){
+        res.render('finds/findError')  
+    }
+
+Imate.findByPk(id).then(imate => {
+    if(imate != undefined) {
+
+        res.render('../views/editImate', {
+            imate: imate
+        })
+
+    } else {
+        res.render('finds/findError')    }
+    }).catch(erro => {
+        res.redirect('/admin/categories')
+})
 })
 
 router.post('/findPerson', (req, res) => {
