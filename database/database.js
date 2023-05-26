9006f80d5b29ecd07a52cd79e8d1d5703cf02a92
Betinsk <gmdust@htomail.com>
2023-05-26 09:38:36 -0300

    Correção de bugs no layout

--- database/database.js
+++ database/database.js
@@ -5,7 +5,6 @@ const Sequelize = require('sequelize')
     dialect: 'mysql'
 })
 */
-
 //const connection = new Sequelize('postgres://3JHyFzxhtt:WI1xwE0y7ZRCyi72fSUdckys@SharedPostgreSQL01A.back4app.com:5433/7dca7222494c4cffa0e7cc4c71792c81') // Example for postgres
 
 const connection = new Sequelize('postgresql://postgres:LRRtScOwvJvpwr3bp5FC@containers-us-west-127.railway.app:6190/railway', {
