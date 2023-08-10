3ee1c370dcc015eb46af8ee2b0fe2b5206a6c58b
Betinsk <60451468+Betinsk@users.noreply.github.com>
2023-08-10 16:16:47 -0300

    Update database.js
    novo banco

--- database/database.js
+++ database/database.js
@@ -7,7 +7,7 @@ const Sequelize = require('sequelize')
 
 //const connection = new Sequelize('postgres://3JHyFzxhtt:WI1xwE0y7ZRCyi72fSUdckys@SharedPostgreSQL01A.back4app.com:5433/7dca7222494c4cffa0e7cc4c71792c81') // Example for postgres
 
-const connection = new Sequelize('postgresql://postgres:LRRtScOwvJvpwr3bp5FC@containers-us-west-127.railway.app:6190/railway', {
+const connection = new Sequelize('postgresql://postgres:Tkrj63Snrkw7IMlB7vBr@containers-us-west-110.railway.app:8047/railway', {
   dialect: 'postgres',
   protocol: 'postgres',
 });
